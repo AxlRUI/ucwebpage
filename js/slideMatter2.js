ad288cd7624561f60b90ec34e5650ab87767249b
AxlRUI <axelpforestrubio@hotmail.com>
2018-06-13 20:10:56 -0500

    Oferta

---
+++ js/slideMatter2.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('.arrowL2').click(function(){
+
+			$(this).parent().css({
+				"margin-left": '0'
+			});
+	});
+	$('.arrowR2').click(function(){
+			$(this).parent().css({
+				"margin-left": "-16.52%"
+			});
+
+	});
+});
